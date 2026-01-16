d4d7419d9fc2387915265f33488263f3de5990c6
Kseniia Iarysh <kseniia.iarysh@orioninc.com>
2026-01-16 13:01:27 -0300

    fixes

--- src/payments/mercadopago_pix.py
+++ src/payments/mercadopago_pix.py
@@ -48,7 +48,7 @@ class MercadoPagoPixProvider:
                 "payment_method_id": "pix",
                 "external_reference": payer_ref,
                 "payer": {
-                    "email": f"{payer_ref}@example.com",
+                    "email": "ksana32@gmail.com",
                     "first_name": "Telegram",
                     "last_name": "User",
                 },
